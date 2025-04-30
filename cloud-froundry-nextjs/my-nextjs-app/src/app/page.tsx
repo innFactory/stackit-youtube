65a26cd9ec6d5d26976dd7fdbaef64da75f7f166
Anton Spöck <a.spoeck@innfactory.de>
2025-04-30 06:33:00 +0200

    fix: packagejson

--- cloud-froundry-nextjs/my-nextjs-app/src/app/page.tsx
+++ cloud-froundry-nextjs/my-nextjs-app/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
-          <li>Save and see your changes instantly!</li>
+          <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className={styles.ctas}>
@@ -34,7 +34,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now!
+            Deploy now
           </a>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -44,12 +44,12 @@ export default function Home() {
           >
             Read our docs
           </a>
-          <img
-              src="https://innfactory-example-bucket-2.object.storage.eu01.onstackit.cloud/image2.png"
-              alt="innFactory logo"
-              width={500}
-            />
         </div>
+        <img
+          src="https://innfactory-example-bucket-2.object.storage.eu01.onstackit.cloud/image2.png"
+          alt="innFactory logo"
+          width={500}
+        />
       </main>
       <footer className={styles.footer}>
         <a
